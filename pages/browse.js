17af610390b2aebfa4f85b13ab0d048931bc80ef
NicholasY4815 <songyang20092009@gmail.com>
2022-12-06 23:50:38 -0500

    debug

--- pages/browse.js
+++ pages/browse.js
@@ -20,18 +20,28 @@ export async function getServerSideProps(context) {
   });
   const data = await res.json();
 
-  const antRes = await fetch('http://127.0.0.1:3001/api/ants/database?user=' + data.user._id, {
-    method: 'GET',
-    headers: {
-      'Cookie': context.req.headers.cookie
-    }
-  });
-  const antData = await antRes.json();
-    
-  return {
-      props: {
-        user: data.user || null,
-        data: antData.data
+  let antRes;
+  if (!data.user) {
+    antRes = await fetch('http://127.0.0.1:3001/api/ants/database?user=' + data.user._id, {
+      method: 'GET',
+      headers: {
+        'Cookie': context.req.headers.cookie
       }
+    });
+  } else {
+    antRes = await fetch('http://127.0.0.1:3001/api/ants/database', {
+      method: 'GET',
+      headers: {
+        'Cookie': context.req.headers.cookie
+      }
+    });
+  }
+  const antData = await antRes.json();
+
+  return {
+    props: {
+      user: data.user || null,
+      data: antData.data
     }
+  }
 }
